bd031e4fb37442f870f3e265b12a271e22d7bae9
Tresor Tshimanga <tshims79@gmail.com>
2022-03-04 13:56:51 +0200

    Use SQl Parameters
    - Added SQL parameters that hard coding the form

--- SensorSystem/Form1.cs
+++ SensorSystem/Form1.cs
@@ -15,12 +15,25 @@ namespace SensorSystem
         private void btnSave_Click(object sender, EventArgs e)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
-            string sqlQuery = "INSERT INTO SENSOR(SensorName, SensorType) VALUES (" + "'" + txtSensorName.Text + "'" + "," + "'" + txtSensorType.Text + "'" + ")";
+            string sqlQuery = "INSERT INTO SENSOR(SensorName, SensorType) VALUES (@sensorname, @sensortype)";
 
             SqlConnection con = new SqlConnection(connectionString);
             con.Open();
-            SqlCommand sc = new SqlCommand(sqlQuery, con);
-            sc.ExecuteNonQuery();
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+            var sensorNameParameter = new SqlParameter("sensorname", System.Data.SqlDbType.VarChar)
+            {
+                Value = txtSensorName.Text
+            };
+            cmd.Parameters.Add(sensorNameParameter);
+
+            var sensorTypeParameter = new SqlParameter("sensortype", System.Data.SqlDbType.VarChar)
+            {
+                Value = txtSensorType.Text
+            };
+            cmd.Parameters.Add(sensorTypeParameter);
+
+            cmd.ExecuteNonQuery();
             con.Close();
         }
     }
